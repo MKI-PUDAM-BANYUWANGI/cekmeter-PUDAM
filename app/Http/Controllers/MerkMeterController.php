7468bac2987947fbb57657ce7ea4cf4a829e823b
kenafsm <kenaffilasyachmaulana@gmail.com>
2024-07-29 09:10:03 +0700

    test

---
+++ app/Http/Controllers/MerkMeterController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MerkMeter;
+use App\Http\Requests\StoreMerkMeterRequest;
+use App\Http\Requests\UpdateMerkMeterRequest;
+
+class MerkMeterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMerkMeterRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MerkMeter $merkMeter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MerkMeter $merkMeter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMerkMeterRequest $request, MerkMeter $merkMeter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MerkMeter $merkMeter)
+    {
+        //
+    }
+}
