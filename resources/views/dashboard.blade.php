29dd81cbbc9da98509490c052d239669b99a9834
Rio Adjie Wiguna <rioadjie100301@gmail.com>
2024-07-29 14:45:32 +0700

    fe update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                class="fas fa-download fa-sm text-white-50"></i> Ekspor Data</a>
     </div>
 
     <!-- Content Row -->
@@ -53,16 +53,16 @@
 
         <!-- Data Staff/User Card -->
         <div class="col-xl-4 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
+            <div class="card border-left-info shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Merk Meter</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">10</div>
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                Data Staff</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">50</div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-tachometer-alt fa-2x text-gray-300"></i>
+                            <i class="fas fa-user fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
